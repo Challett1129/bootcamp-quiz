8a3caccb5c735945f2a5b992a46ddec2286753de
Collin <collinhallett@gmail.com>
2021-07-13 20:38:57 -0500

    added pseudocode to js

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+//array to hold all question objects 
+const quizQuestions = {
+    question: 'select thisC',
+    answers: {
+        a: 'thisA',
+        b: 'thisB',
+        c: 'thisC',
+        d: 'thisD' 
+    },
+    correctAnswer: 'thisC'
+}
+//create a function to create elements that display as a quiz on the page. Display questions as radio 
+const quizEl = function() {
+    let quizDisplay = document.createElement("div");
+}
+
+//function to iterate through questions to send through quizEl
+const quiz = function(){
+    quizQuestions.forEach(quizQuestions.question, quizQuestions)
+}
+
+//create submit button
+
+//function to save and store high scores
+
+//create function  to display high scores
+
+//create listen function to listen for click on submit
