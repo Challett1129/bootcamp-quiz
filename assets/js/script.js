e46d6db3b403284f85dd668f7489244db4e45d85
Collin <collinhallett@gmail.com>
2021-07-20 17:07:04 -0500

    worked with a tutor, got the start button, right and wrong, and quiz questions working

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,10 @@ const headerEl = document.querySelector("#header");
 const pageContentEl = document.querySelector("#page-content");
 const startBtn = document.querySelector("#start-btn");
 //array to hold all question objects 
+//high score array
+const highScoreArr = []
+
+
 const quizQuestions = [ 
 {
     question: 'select thisC',
@@ -30,56 +34,100 @@ const quizQuestions = [
     correctAnswer: "b"
 }
 ];
-
+let currentQuestion = 0;    
 //start button to create the quiz 
 let quizHandler = (event) => {
     //remove current text to display questions
     let startStr = document.querySelector("#quiz");
-    startStr.remove();
-    startBtn.remove();
-
-    //variable to tell what question to display
-    let currentQuestion = 0; 
-
-    //main function which cycles through quiz
-    if(currentQuestion < quizQuestions.length) {
-        let quizQuestionEl = document.createElement("div");
-        quizQuestionEl.className = "quiz-form"; 
-        quizQuestionEl.innerHTML = 
-        '<h1 class = "question" id = "question">' + quizQuestions[currentQuestion].question + '</h1>' + 
-        '<form id="quiz">' +
-        '<input type="radio" id="0" name="option" value="a">' +
-        '<label for="0">' + quizQuestions[currentQuestion].answers.a + '</label><br>' +
-
-        '<input type="radio" id="1" name="option" value="b">' +
-        '<label for="1">' + quizQuestions[currentQuestion].answers.b + '</label><br>' +
-
-        '<input type="radio" id="2" name="option" value="c">' + 
-        '<label for="2">' + quizQuestions[currentQuestion].answers.c + '</label><br>' +
-
-        '<input type="radio" id="3" name="option" value="d">' + 
-        '<label for="3">' + quizQuestions[currentQuestion].answers.d + '</label><br>' +
-
-        '<p id="message"></p>' +
-
-        '</form>'
-        
-        let answer = document.getElementsByName("option");
-        console.log(answer);
-
-        //add code to read user input in radio and display message if user is right or wrong
-        //.checked method to check what radio input is selected
-        //if .checked is correct display correct and go to next question else display incorrect and -10 seconds to timer, then next question
-        //at end of quiz create element prompt to submit initials then store high score 
-        //next screen displays all high-scores with an option to restart the quiz
-        quizContainerEl.appendChild(quizQuestionEl);
-        }
-
-
-
-        
-        
+    startStr.classList.add("hide");
+    startBtn.classList.add("hide");
+    
+    createQuiz();
+    
 }
+
+checkAnswer = function(event) {
+    if(event.target.name === quizQuestions[currentQuestion].correctAnswer) {
+        currentQuestion++;
+        createQuiz();
+    } 
+    else {
+        timer -=10;
+        currentQuestion++;
+        createQuiz();
+    }
+}
+
+createQuiz = function() {
+    quizContainerEl.innerHTML = "";
+    if(quizQuestions.length > currentQuestion) {  
+
+        //creates the question 
+        const questionEl = document.createElement("h3");
+        questionEl.textContent = quizQuestions[currentQuestion].question; 
+        
+        //creates answer buttons
+        const answerElA = document.createElement("button");
+        answerElA.className = "btn"; 
+        answerElA.name = "a";
+        answerElA.textContent = quizQuestions[currentQuestion].answers.a
+        
+        const answerElB = document.createElement("button");
+        answerElB.className = "btn";
+        answerElB.name = "b";
+        answerElB.textContent = quizQuestions[currentQuestion].answers.b
+        
+        const answerElC = document.createElement("button");
+        answerElC.className = "btn";
+        answerElC.name = "c";
+        answerElC.textContent = quizQuestions[currentQuestion].answers.c
+        
+        const answerElD = document.createElement("button");
+        answerElD.className = "btn";
+        answerElD.name = "d";
+        answerElD.textContent = quizQuestions[currentQuestion].answers.d
+
+        // const answerLiA = document.createElement("li"); 
+        // const answerLiB = document.createElement("li");
+        // const answerLiC = document.createElement("li");
+        // const answerLiD = document.createElement("li");                     
+        
+        const answerUlEl = document.createElement("ul")
+        
+        
+        answerUlEl.append(answerElA, answerElB, answerElC, answerElD)
+        quizContainerEl.appendChild(questionEl); 
+        quizContainerEl.appendChild(answerUlEl); 
+
+        let optionsArr = document.querySelectorAll(".btn")
+        for(i = 0; i < optionsArr.length; i++) {
+            optionsArr[i].addEventListener("click", checkAnswer)
+        }
+}
+}
+
+highScore = function() {
+
+
+
+}
+
+
+
+
+
+
+        
+        
+
+
+
+        
+        
+
+
+
+
     
 
 
@@ -88,9 +136,13 @@ let quizHandler = (event) => {
 function countdown() {
 	timer--;
 	document.getElementById("seconds").innerHTML = String("Time: " + timer );
-	if (timer > 0) {
+	if (timer > 0 && currentQuestion  < quizQuestions.length) {
 		setTimeout(countdown, 1000);
 	}
+    else 
+    {
+        highScore();
+    }
 };
 
 countdown();
